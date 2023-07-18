57453134ce0a2defbb44bb3de16e5c8d3463c426
Toby Hsieh <thrasher13b@gmail.com>
2023-07-17 23:22:44 -0700

    Create NotificationListener (NotificationListenerService subclass)

---
+++ app/src/main/java/io/github/tobyhs/trackannouncer/NotificationListener.kt
@@ -0,0 +1,9 @@
+package io.github.tobyhs.trackannouncer
+
+import android.service.notification.NotificationListenerService
+
+/**
+ * A notification listener service for [android.media.session.MediaSessionManager] to detect media
+ * sessions
+ */
+class NotificationListener : NotificationListenerService()
