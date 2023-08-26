ed5207e4f11d583e5de006b366bc9bb7e2fcb106
Toby Hsieh <thrasher13b@gmail.com>
2023-08-26 15:05:22 -0700

    Create TrackAnnouncerService
    This is the main foreground service that sets things up to detect when
    the track title changes and speaks the track title.

--- app/src/main/java/io/github/tobyhs/trackannouncer/TrackAnnouncerService.kt
+++ app/src/main/java/io/github/tobyhs/trackannouncer/TrackAnnouncerService.kt
@@ -4,9 +4,16 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+import android.media.AudioManager
+import android.media.session.MediaSessionManager
 import android.os.IBinder
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+
+import androidx.annotation.VisibleForTesting
 
 /**
  * Service that speaks the track title when a track starts playing
@@ -17,12 +24,27 @@ class TrackAnnouncerService : Service() {
         internal const val NOTIFICATION_CHANNEL_ID = "service"
     }
 
+    private lateinit var audioManager: AudioManager
+    @VisibleForTesting internal lateinit var textToSpeech: TextToSpeech
+    private lateinit var mediaSessionManager: MediaSessionManager
+    private lateinit var sessionsChangedListener: SessionsChangedListener
+
     override fun onCreate() {
         super.onCreate()
         foregroundSelf()
+        audioManager = getSystemService(AudioManager::class.java)
+        initTextToSpeech()
+        initMediaSessionManager()
     }
 
     override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onDestroy() {
+        mediaSessionManager.removeOnActiveSessionsChangedListener(sessionsChangedListener)
+        sessionsChangedListener.unregisterCallbacks()
+        textToSpeech.shutdown()
+        super.onDestroy()
+    }
 
     private fun foregroundSelf() {
         val channel = NotificationChannel(
@@ -36,5 +58,27 @@ class TrackAnnouncerService : Service() {
             .setOngoing(true)
             .build()
         startForeground(NOTIFICATION_ID, notification, FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK)
+    }
+
+    private fun initTextToSpeech() {
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status != TextToSpeech.SUCCESS) {
+                Toast.makeText(this, R.string.tts_init_failure, Toast.LENGTH_LONG).show()
+                stopSelf()
+            }
+        }
+        textToSpeech.setOnUtteranceProgressListener(ResumePlaySpeechListener(audioManager))
+    }
+
+    private fun initMediaSessionManager() {
+        mediaSessionManager = getSystemService(MediaSessionManager::class.java)
+        val listenerComponent = ComponentName(this, NotificationListener::class.java)
+        sessionsChangedListener = SessionsChangedListener(
+            MetadataChangedCallback(textToSpeech, audioManager)
+        )
+        mediaSessionManager.addOnActiveSessionsChangedListener(
+            sessionsChangedListener,
+            listenerComponent
+        )
     }
 }
--- app/src/test/java/io/github/tobyhs/trackannouncer/TrackAnnouncerServiceTest.kt
+++ app/src/test/java/io/github/tobyhs/trackannouncer/TrackAnnouncerServiceTest.kt
@@ -2,6 +2,13 @@ package io.github.tobyhs.trackannouncer
 
 import android.app.NotificationManager
 import android.content.Intent
+import android.media.session.MediaController
+import android.media.session.MediaSessionManager
+import android.speech.tts.TextToSpeech
+
+import io.mockk.justRun
+import io.mockk.mockk
+import io.mockk.verify
 
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.nullValue
@@ -13,14 +20,14 @@ import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadows.ShadowMediaSessionManager
+import org.robolectric.shadows.ShadowToast
 
 @RunWith(RobolectricTestRunner::class)
 class TrackAnnouncerServiceTest {
     @Test
     fun `onCreate foregrounds itself`() {
-        val serviceController = Robolectric.buildService(TrackAnnouncerService::class.java)
-        try {
-            val service = serviceController.create().get()
+        runService { service ->
             assertThat(shadowOf(service).isLastForegroundNotificationAttached, equalTo(true))
 
             val notificationManager = service.getSystemService(NotificationManager::class.java)
@@ -36,13 +43,67 @@ class TrackAnnouncerServiceTest {
             val channelId = notification.notification.channelId
             assertThat(channelId, equalTo(TrackAnnouncerService.NOTIFICATION_CHANNEL_ID))
             assertThat(notification.isOngoing, equalTo(true))
-        } finally {
-            serviceController.destroy()
+        }
+    }
+
+    @Test
+    fun `onCreate creates a TextToSpeech`() {
+        runService { service ->
+            val ttsShadow = shadowOf(service.textToSpeech)
+            assertThat(ttsShadow.utteranceProgressListener is ResumePlaySpeechListener, equalTo(true))
+
+            ShadowToast.reset()
+            ttsShadow.onInitListener.onInit(TextToSpeech.ERROR)
+            val lastToastText = ShadowToast.getTextOfLatestToast()
+            assertThat(lastToastText, equalTo("Track Announcer: Failed to initialize TextToSpeech"))
+            assertThat(shadowOf(service).isStoppedBySelf, equalTo(true))
+        }
+    }
+
+    @Test
+    fun `onCreate adds a SessionsChangedListener to the MediaSessionManager`() {
+        runService { service ->
+            val msmShadow = shadowOf(service.getSystemService(MediaSessionManager::class.java))
+            val controller: MediaController = mockk()
+            justRun { controller.registerCallback(any()) }
+            justRun { controller.unregisterCallback(any()) }
+            msmShadow.addController(controller)
+            verify { controller.registerCallback(ofType(MetadataChangedCallback::class)) }
         }
     }
 
     @Test
     fun `onBind returns null`() {
         assertThat(TrackAnnouncerService().onBind(Intent()), nullValue())
+    }
+
+    @Test
+    fun onDestroy() {
+        val controller: MediaController = mockk()
+        justRun { controller.registerCallback(any()) }
+        justRun { controller.unregisterCallback(any()) }
+        lateinit var msmShadow: ShadowMediaSessionManager
+
+        val service = runService { service ->
+            msmShadow = shadowOf(service.getSystemService(MediaSessionManager::class.java))
+            msmShadow.addController(controller)
+        }
+
+        val anotherController: MediaController = mockk()
+        msmShadow.addController(anotherController)
+        verify(exactly = 0) { anotherController.registerCallback(any()) }
+        verify { controller.unregisterCallback(ofType(MetadataChangedCallback::class)) }
+        assertThat(shadowOf(service.textToSpeech).isShutdown, equalTo(true))
+    }
+
+    private fun runService(action: (TrackAnnouncerService) -> Unit): TrackAnnouncerService {
+        val controller = Robolectric.buildService(TrackAnnouncerService::class.java)
+        val service = controller.create().get()
+        try {
+            action(service)
+        } finally {
+            controller.destroy()
+        }
+        return service
     }
 }
