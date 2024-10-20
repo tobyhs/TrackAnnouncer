46ceeab588ea8b4b300e3b1269c01e0c60e6a3fe
Toby Hsieh <thrasher13b@gmail.com>
2024-10-19 22:34:52 -0700

    Upgrade Java to version 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,16 +36,16 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
         viewBinding = true
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        jvmToolchain(21)
     }
 
     testOptions {
