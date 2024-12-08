2ee51987891a9d749e4b7010d9daf4bc36ab8610
Toby Hsieh <thrasher13b@gmail.com>
2024-12-07 22:09:28 -0800

    Upgrade Kotlin to 1.9.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,5 @@ allprojects {
 
 plugins {
     id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
