5ea48ecd9d3faccf3c22b9756f9097a211ba81f5
Toby Hsieh <thrasher13b@gmail.com>
2024-12-07 22:04:14 -0800

    Upgrade AGP to 8.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.7.1" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 }
