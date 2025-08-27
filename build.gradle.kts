b7c8e2ce4154b6e60ce93e9acf77bd76bf01d364
Toby Hsieh <thrasher13b@gmail.com>
2025-08-26 20:13:09 -0700

    Upgrade Gradle to 8.13 & AGP to 8.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ allprojects {
 }
 
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.12.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
